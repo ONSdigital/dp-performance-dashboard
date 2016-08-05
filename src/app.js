dd335584ae5466a3da57062c11338af8c611ab0c
Crispioso <crispin.merriman@gmail.com>
2016-08-05 15:49:40 +0100

    Initial API and state layers working, bug in request latest data since update to fetch both objects

--- src/app.js
+++ src/app.js
@@ -1,4 +1,57 @@
+/** Configuration file for application */
 
-require('./js/api.js');
+
+/* Import other JS files */
+var api = require('./js/api'),
+    store = require('./js/state');
 require('./js/views');
-require('../node_modules/redux/dist/redux.min');
+
+
+/* Initialise data */
+api.initialiseData();
+
+/* Subscribe to state changes */
+store.subscribe(function() {
+    // console.log("State update: ", store.getState());
+});
+
+
+
+/* Throw away view JS for testing API/state JS */
+var dataBtn = document.getElementById('latest-data');
+dataBtn.addEventListener('click', function() {
+    api.getTrafficData();
+});
+
+var viewBtn = document.getElementById('toggle-active');
+viewBtn.addEventListener('click', function() {
+    var currentState = store.getState(),
+        toggledView;
+
+    if (currentState.activeView == 'traffic') {
+        toggledView = 'technical'
+    } else if (currentState.activeView == 'technical') {
+        toggledView = 'traffic'
+    } else {
+        toggledView = 'traffic'
+    }
+
+    store.dispatch({
+        type: 'REQUEST_VIEW',
+        view: toggledView
+    })
+});
+
+// Listen for changes to view and renders view with newly received data
+store.subscribe(function() {
+    var currentState = store.getState(),
+        activeView = currentState.activeView,
+        viewElement = document.getElementById('active-view');
+
+    if (currentState[activeView].status == 'RECEIVED' && currentState.viewChange == 'IN_PROGRESS') {
+        store.dispatch({
+            type: 'UPDATED_VIEW'
+        });
+        viewElement.innerHTML = activeView;
+    }
+});
