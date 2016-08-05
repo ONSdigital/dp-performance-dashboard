ccd649ee01b1e2915adee1d36f616364341d23b8
Jon Jones <jondewijones@gmail.com>
2016-08-05 15:31:10 +0100

    Get hash from uri and pass to view controller

--- src/app.js
+++ src/app.js
@@ -1,4 +1,9 @@
 
+// get the uri hash for routing
+var uriHash = window.location.hash ? window.location.hash.substring(1) : false;
+
 require('./js/api.js');
-require('./js/views');
 require('../node_modules/redux/dist/redux.min');
+var view = require('./js/views');
+
+view.init(uriHash);
