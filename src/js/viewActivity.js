240b9a040a9c0be9d4e0c52f964bd7f5f1f0eca1
jondewijones <jondewijones@gmail.com>
2016-08-08 17:51:16 +0100

    Remove comments

--- src/js/viewActivity.js
+++ src/js/viewActivity.js
@@ -35,29 +35,5 @@ var viewActivity = {
 //         data: [5, 7, 3]
 //     }]
 // });
-//
-// Highcharts.chart('chart--js-2', {
-//     chart: {
-//         type: 'bar'
-//     },
-//     title: {
-//         text: 'Meat eaten'
-//     },
-//     xAxis: {
-//         categories: ['Lamb', 'Chicken', 'Beef', '"Quorn"']
-//     },
-//     yAxis: {
-//         title: {
-//             text: 'Meat eaten'
-//         }
-//     },
-//     series: [{
-//         name: 'Crispin',
-//         data: [0, 0, 0, 50]
-//     }, {
-//         name: 'Jon',
-//         data: [34, 48, 34, 0]
-//     }]
-// });
 
 module.exports = viewActivity;
