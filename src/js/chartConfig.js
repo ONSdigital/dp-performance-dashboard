ebc5353559d355dbd6e0046060f4850217537000
Jon Jones <jondewijones@gmail.com>
2016-08-10 10:36:14 +0100

    Remove default data

--- src/js/chartConfig.js
+++ src/js/chartConfig.js
@@ -1,22 +1,22 @@
 var chartConfig = {
     chart: {
-        type: 'line'
+        type: ''
     },
     colors: ['#55A9DC', '#E6645C', '#886DB3', '#6CC080'],
     title: {
         text: ''
     },
     xAxis: {
-        categories: [1470319946,1470319886,1470319826,1470319766,1470319706,1470319646,1470319586,1470319526,1470319466,1470319406,1470319346,1470319286,1470319226,1470319166,1470319106,1470319046]
+        categories: []
     },
     yAxis: {
         title: {
-            text: 'Fruit eaten'
+            text: ''
         }
     },
     series: [{
-        name: 'Response time',
-        data: [195,473,255,152,218,2129,504,100,892,327,648,266,725,81,247,204],
+        name: '',
+        data: [],
         marker : {
             enabled: true,
             lineWidth: 3,
